39bfd6ec62b38c8343470bf8a7948add059c798a
Konstantinos Loupasakis <loupax@gmail.com>
2026-04-19 15:37:51 +0200

    Removed redundant icon

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@ static const Block blocks[] = {
 	/* icon   command                                         interval  signal */
 	{"󰂚 ",  "$HOME/src/dotfiles/someblocks/scripts/sb-notifications",         0,        4},
 	{"󰑊 ",  "$HOME/src/dotfiles/someblocks/scripts/sb-recorder",              1,        0},
-	{"󰎆 ",  "$HOME/src/dotfiles/someblocks/scripts/sb-media",                 0,        3},
+	{"",  "$HOME/src/dotfiles/someblocks/scripts/sb-media",                   0,        3},
 	{"󰕾 ",  "$HOME/src/dotfiles/someblocks/scripts/sb-volume",                0,        2},
 	{"󰤨 ",  "$HOME/src/dotfiles/someblocks/scripts/sb-network",               2,        0},
 	{"󰻠 ",  "$HOME/src/dotfiles/someblocks/scripts/sb-cpu",                   2,        0},
